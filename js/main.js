2be5eb062f2a2edb0f9d2fd1e6c4e71280c741e8
SandeepRaparthi1 <SandeepRaparthi1@github.com>
2015-08-27 01:09:50 +0530

    Added Authentication

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<script src="../vendor/jquery-2.1.4.min.js"></script>
 function loadManifest() {
     if (sessionStorage && !sessionStorage.getItem("serverUrl"))
     {
@@ -8,4 +9,13 @@ function loadManifest() {
             console.log("reading manifest file request Failed: " + err);
         });
     }
+}
+function CheckAuthentication() {
+	if (sessionStorage && !sessionStorage.getItem("serverUrl"))
+    { 
+        $.ajax({url: "http://localhost:8080/openmrs/ws/rest/v1/session",headers: '{"accept": "application/json"}', success: function(result){
+        sessionStorage.setItem("authentication", result.detail.response.authenticated;     
+    }          
+    });
+});
 }
